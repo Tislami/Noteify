79c074f80c911c0b938a7e3c2cf82dcb0fd61689
Tariyel <islami.tariyel@gmail.com>
2023-03-07 14:21:39 +0400

    Initial commit

---
+++ Noteify/NoteifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteifyApp.swift
+//  Noteify
+//
+//  Created by Tariyel Islami on 7.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct NoteifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
